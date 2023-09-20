20c68931931f00459ffc585e5588e500536ce011
AngelBottomless <aria1th@naver.com>
2023-09-20 18:40:41 +0900

    add some test functions

--- library/test_utils.py
+++ library/test_utils.py
@@ -76,11 +76,12 @@ def process_test_latent_couple(apiInstance: WebUIApi, control_ref_path:str, mask
     )
     return result_control_latent_couple
 
-def wait_for_result(result:QueuedTaskResult, result_list:list):
+def wait_for_result(result:QueuedTaskResult, result_list:list,
+                    check_interval:int=1):
     """wait for result to finish"""
     # function for thread
     while not result.is_finished():
-        time.sleep(1)
+        time.sleep(check_interval)
     result_list.append(result)
 
 def process_test_and_wait(apiInstance:WebUIApi, control_ref_path:str, mask_ref_path:str, result_list:list, should_wait_finish:bool=True,
@@ -92,3 +93,124 @@ def process_test_and_wait(apiInstance:WebUIApi, control_ref_path:str, mask_ref_p
     if should_wait_finish:
         thread.join()
     return result_control_latent_couple
+
+
+def concat_image_horizontally(img_list:list):
+    """
+    Concat images horizontally
+    """
+    max_width = max([img.width for img in img_list])
+    max_height = max([img.height for img in img_list])
+    width = max_width * len(img_list)
+    height = max_height
+    concat_image = Image.new("RGB", (width, height))
+    offset_x = 0
+    for img in img_list:
+        target_ratio = max_width / img.width
+        target_height = int(img.height * target_ratio)
+        resized_img = img.resize((max_width, target_height))
+        concat_image.paste(resized_img, (offset_x, 0))
+        offset_x += max_width
+        
+    return concat_image
+
+
+  
+def test_mask(instance,
+            prompt_1_arg = "1boy",
+            prompt_2_arg = "1girl",
+            lora_stop_steps = (10, 10),
+            extra_args = {},
+            mask_path = "test_mask.png",
+            controlnet_path = "test_controlnet.png",
+            controlnet_preprocessor = "none",
+            controlnet_model = "control_v11p_sd15_openpose [cab727d4]",
+            base_prompt = "masterpiece, night, line art, simple background, street",
+            common_prompt = "(best-quality:0.8), detailed face, perfect anime illustration",
+            negative_prompt = "(worst quality:0.8), verybadimagenegative_v1.3, easynegative, (surreal:0.8), (modernism:0.8), (art deco:0.8), (art nouveau:0.8)",
+        ):
+    """
+    Tests regional prompter with mask mode.
+    Arguments :
+        prompt_1_arg : prompt for first region
+        prompt_2_arg : prompt for second region
+        lora_stop_steps : tuple of (stop_step, stop_step_hires)
+        extra_args : extra arguments for txt2img_task
+        
+    Returns :
+        QueuedTaskResult object
+        (use QueuedTaskResult.get_image() to get image)
+    """
+    result_container = []
+    extra_args_base = {
+        'enable_hr':True,
+        'hr_upscaler':'R-ESRGAN 4x+ Anime6B',
+        'width':768,
+        'height':1152,
+        'hr_scale':1.3,
+        'denoising_strength':0.6
+    }
+    extra_args_base.update(extra_args)
+    result = instance.txt2img_task(
+        # assume 3-region image
+        prompt=f"""{base_prompt} BREAK
+        {common_prompt}, {prompt_2_arg} BREAK
+        {common_prompt}, {prompt_1_arg} """,
+        negative_prompt=negative_prompt,
+        #prompt = "masterpiece, night BREAK red BREAK blue BREAK yellow",
+        controlnet_units=[
+                ControlNetUnit(input_image=open_controlnet_image(controlnet_path),
+                               module=controlnet_preprocessor, model=controlnet_model)
+        ],
+        alwayson_scripts={
+            "regional prompter" : {
+                "args" : [
+                    True, # Active
+                    False, # debug
+                    'Mask', # can be Matrix, Mask, Prompt
+                    "Horizontal", # can be Horizontal, Vertical, Columns, Rows
+                    "Mask", # not used
+                    "Prompt", # not used
+                    "1, 1", # Divide Ratio
+                    "0.2", # Base Ratio
+                    False, # Use base prompt
+                    False, # Use common prompt
+                    False, # Use common negative prompt
+                    "Latent", # Attention, Latent
+                    False, # disable convert 'AND' to 'BREAK'
+                    "0", # LoRA in negative textencoder
+                    "0", # LoRA in negative U-net
+                    "0", # threshold
+                    raw_b64_img(open_mask_image(mask_path)), # base64 encoded image or 'absolute path' if saved on server
+                    str(lora_stop_steps[0]), # LoRA stop step
+                    str(lora_stop_steps[1]), # LoRA Hires stop step
+                    False, # flip "," and ";"
+                ]
+            }
+        },
+        **extra_args_base
+    )
+    wait_for_result(result, result_container)
+    return result_container[0]
+
+def test_and_save(args_1:list = [], args_2:list = [], suffix = "", filepath = './examples',
+                mask_path:str = "",
+                controlnet_path:str = "",
+                prompts:dict = {}):
+    """
+    Tests mask generation and save result to filepath
+    """
+    assert os.path.exists(mask_path), "mask_path does not exist"
+    assert os.path.exists(controlnet_path), "controlnet_path does not exist"
+    if not os.path.exists(filepath):
+        os.makedirs(filepath, exist_ok=True)
+    for prompt_1 in args_1:
+        for prompt_2 in args_2:
+            p1 = prompts[prompt_1]
+            p2 = prompts[prompt_2]
+            result = test_mask(p1, p2,
+                               mask_path = mask_path,
+                               controlnet_path = controlnet_path,
+                               ).get_image()
+            result.save(os.path.join(filepath, f"{prompt_1}_{prompt_2}_{suffix}.png"))
+            
